18f7334e519a0922ef8f3b1a6d01bdd243b7531c
Madhur Garg <madhur@hipbar.com>
2019-04-23 18:24:21 +0530

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {Switch} from "react-router-dom"
+import { Switch } from "react-router-dom"
 import { Route } from "react-router-dom"
 import { createBrowserHistory } from "history"
 import { Router } from "react-router"
+import Welcome from "./Welcome"
+import Layout from "./Layout"
+import ListConsumers from "./ListConsumers"
 
 const history = createBrowserHistory()
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router history={history}>
+function App() {
+  return (
+    <Router history={history}>
+      <Layout>
         <Switch>
           <Route
-            exact 
-            path="/" 
-            component={<p></p>}
+            exact
+            path="/"
+            component={ListConsumers}
+          />
+          <Route
+            exact
+            path="/consumers"
+            component={ListConsumers}
           />
         </Switch>
-      </Router>
-    )
-  }
+      </Layout>
+    </Router>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("app"))
