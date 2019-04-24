dbfbbd4eb9ced1053197aa90cd7374e17a8ffa8b
Madhur Garg <madhur@hipbar.com>
2019-04-25 02:04:31 +0530

    added update consumer, consumer detail and consumer soa endpoints

--- src/Api.js
+++ src/Api.js
@@ -1,9 +1,35 @@
-import { POST } from "Utils/fetch";
+import { POST, GET } from "Utils/fetch";
 
 export function fetchConsumers(req) {
   return POST({
     api: "/Api/consumer/list",
     apiBase: "customer",
     data: req
-  }).then(json => json);
+  })
+    .then(json => json);
+}
+
+export function updateConsumer(req) {
+  return POST({
+    api: "/Api/consumer/update",
+    apiBase: "customer",
+    data: req
+  })
+    .then(json => json)
+}
+
+export function fetchConsumerDetail(req) {
+  return GET({
+    api: `/Api/consumer/details/${req.consumer_id}`,
+    apiBase: "customer"
+  })
+    .then(json => json)
+}
+
+export function fetchConsumerSOA(req) {
+  return POST({
+    api: `/Api/consumer/soa/list/${req.consumer_id}`,
+    apiBase: "customer",
+    data: req
+  })
 }
