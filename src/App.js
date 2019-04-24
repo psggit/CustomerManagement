4e35a4caa70dd3a5d85ab43aa60e11f30b5e668f
Madhur Garg <madhur@hipbar.com>
2019-04-25 02:04:56 +0530

    added consumer soa and consumer detail route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Router } from "react-router"
 import Welcome from "./Welcome"
 import Layout from "./Layout"
 import ListConsumers from "./ListConsumers"
+import ConsumerDetail from "./ConsumerDetail"
+import ConsumerSOA from "./ConsumerSOA"
 import "Components/Pagination/pagination.scss"
 
 const history = createBrowserHistory()
@@ -25,6 +27,16 @@ function App() {
             exact
             path="/consumers"
             component={ListConsumers}
+          />
+          <Route
+            exact
+            path="/consumers/:consumer_id"
+            component={ConsumerDetail}
+          />
+          <Route
+            exact
+            path="/consumers/soa/:consumer_id"
+            component={ConsumerSOA}
           />
         </Switch>
       </Layout>
