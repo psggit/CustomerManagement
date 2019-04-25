fe5baca7f7bb2342e710909f3bfa0640f7bad5cd
Madhur Garg <madhur@hipbar.com>
2019-04-25 19:05:58 +0530

    removed unsued menuitems

--- src/components/sidemenu/index.js
+++ src/components/sidemenu/index.js
@@ -3,12 +3,12 @@ import "./sidenav.scss"
 
 const items = [
   { label: "List Consumer", href: "/consumers" },
-  { label: "Upload KYC", href: "/consumer/update" },
-  { label: "Consumer Detail", href: "/consumer/detail" },
-  { label: "Consuemr SOA", href:"/consumer/soa" },
-  { label: "Consumer Issues", href: "/consumer/issues" },
-  { label: "Consumer Notes", href: "/consumer/notes" },
-  { label: "Create Note", href: "/consumer/notes/create" }
+  // { label: "Upload KYC", href: "/consumer/update" },
+  // { label: "Consumer Detail", href: "/consumer/detail" },
+  // { label: "Consuemr SOA", href:"/consumer/soa" },
+  // { label: "Consumer Issues", href: "/consumer/issues" },
+  // { label: "Consumer Notes", href: "/consumer/notes" },
+  // { label: "Create Note", href: "/consumer/notes/create" }
 ]
 
 function SideMenu() {
