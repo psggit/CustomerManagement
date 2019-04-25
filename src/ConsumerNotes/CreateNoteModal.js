b08edcb9387f99803af255c8dc77750768f82ab7
Madhur Garg <madhur@hipbar.com>
2019-04-25 18:53:42 +0530

    modalbox for creating a new note

---
+++ src/ConsumerNotes/CreateNoteModal.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react"
+import TitleAndSave from "Components/ModalBox/TitleAndSave"
+import "./CreateNoteModal.scss"
+import { fetchNoteIssues, createConsumerNote } from "../Api";
+
+export default function CreateNoteModal({ consumer_id }) {
+  return class CreateNoteModal extends React.Component {
+    constructor() {
+      super()
+      const consumer_id = parseInt(location.pathname.split("/").pop())
+      this.state = {
+        consumer_id,
+        issueId: null,
+        noteDescription: "",
+        noteIssues: []
+      }
+      this.handleSave = this.handleSave.bind(this)
+      this.setNoteIssues = this.setNoteIssues.bind(this)
+      this.setIssueId = this.setIssueId.bind(this)
+    }
+    handleSave() {
+      const { consumer_id, issueId, noteDescription } = this.state
+      const createConsumerNoteReq = {
+        consumer_id,
+        issue_id: issueId,
+        description: noteDescription
+      }
+      if (noteDescription.length) {
+        createConsumerNote(createConsumerNoteReq)
+          .then(json => {
+            alert(json.message)
+          })
+      }
+    }
+    setNoteIssues(noteIssues) {
+      this.setState({ noteIssues })
+    }
+    setIssueId(issueId) {
+      this.setState({ issueId })
+    }
+    setNoteDescription(noteDescription) {
+      this.setState({ noteDescription })
+    }
+    componentDidMount() {
+      fetchNoteIssues()
+        .then(fetchNoteIssuesRes => {
+          this.setNoteIssues(fetchNoteIssuesRes.issue)
+          this.setIssueId(fetchNoteIssuesRes.issue[0].id)
+        })
+    }
+    render() {
+      return (
+        <TitleAndSave title="Create Note" handleSave={this.handleSave}>
+          <select onChange={(e) => { this.setIssueId(e.target.value) }}>
+            {
+              this.state.noteIssues.map((item, i) => (
+                <option key={item.id} value={item.id}>{item.code}</option>
+              ))
+            }
+          </select>
+          <textarea onChange={(e) => { this.setNoteDescription(e.target.value) }}></textarea>
+        </TitleAndSave>
+      )
+    }
+  }
+}
