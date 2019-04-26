9ec124b977b0fb7f1ebf527fc2f1065100fc032f
Madhur Garg <madhur@hipbar.com>
2019-04-26 17:44:53 +0530

    added login page

---
+++ src/Login/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react"
+import { Form, FormGroup } from "Components/Form"
+import Input from "Components/Input"
+import Button from "Components/Button"
+import { authLogin, authTokenInfo } from "../Api"
+import { createSession } from "Utils/session"
+import "./login.scss"
+
+export default function Login(props) {
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoading, setLoadingState] = useState(false)
+
+  const handleClick = e => {
+    e.preventDefault()
+    const authLoginReq = {
+      username: userName,
+      password: password
+    }
+
+    if (userName.length > 0 && password.length > 0) {
+      setLoadingState(true)
+      /** Get the token and create a session */
+      authLogin(authLoginReq)
+        .then(json => {
+          authTokenInfo()
+            .then(json => {
+              setLoadingState(false)
+              createSession(JSON.stringify(json))
+              props.history.push("/admin")
+            })
+        })
+        .catch(err => {
+          console.log(err)
+          setLoadingState(false)
+        })
+    }
+  }
+
+  /** Check if already logged in */
+  useEffect(() => {
+    authTokenInfo()
+      .then(json => {
+        props.history.push("/admin")
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  return (
+    <div id="login--page">
+      <div>
+        <h2>Sign in to Customer Management</h2>
+        <div className="login--page__form">
+          <Form>
+            <FormGroup>
+              <label>Username</label>
+              <Input onChange={(e) => { setUserName(e.target.value) }} />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Password</label>
+              <Input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+            </FormGroup>
+
+            <Button disabled={isLoading} onClick={handleClick}>Sign in</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
+}
