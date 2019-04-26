5cdfdbe73f01aeb7fe55aef057bc946fe1375a67
Madhur Garg <madhur@hipbar.com>
2019-04-26 17:48:26 +0530

    added compression and caching for js bundles

--- server/index.js
+++ server/index.js
@@ -1,45 +1,28 @@
 const express = require("express")
-// const request = require("request")
 const path = require("path")
-// const fs = require("fs")
-// const React = require("react")
-// const { renderToNodeStream } = require("react-dom/server")
-
-// const Albums = require("./../dist-ssr/Albums").default
 const app = express()
 
-
-// app.get("/", (req, res) => {
-//   const html = fs.readFileSync(path.resolve(__dirname, `./../dist/ssr.html`), "utf-8")
-//   const [head, tail] = html.split("{content}")
-//   res.write(head)
-//   const url = "https://jsonplaceholder.typicode.com/photos?_page=1&&_limit=10"
-//   request({
-//     method: "GET",
-//     url
-//   }, (err, httpsRes, body) => {
-//     const newTail = tail.split("{script}")
-//       .join(`
-//       <script id="ssr__script">
-//         window.__ALBUMS__ = ${JSON.stringify(body)}
-//       </script>
-//       `)
-
-//       const reactElement = React.createElement(Albums, { albums: JSON.parse(body) })
-//       const stream = renderToNodeStream(reactElement)
-//       stream.pipe(res, { end: false })
-//       stream.on("end", () => {
-//         res.write(newTail)
-//         res.end()
-//       })
-//   })
-// })
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./../dist/index.html"))
+app.get("*.js", (req, res, next) => {
+  console.log("Processing js files....")
+  console.log("Gzipping....")
+  if (/app.*.js/.test(req.url) || /vendor.*.js/.test(req.url)) {
+    req.url += ".gz"
+    res.set("Content-Encoding", "gzip")
+    res.set("Content-type", "text/javascript")
+  }
+  const vendorUrlRegex = /vendor.*.js/
+  if (vendorUrlRegex.test(req.url)) {
+    console.log("Setting cache for vendor....")
+    res.setHeader("Cache-Control", "private, max-age=31536000")
+  }
+  next()
 })
 
-app.use(express.static(path.join(__dirname, "./../dist")))
+app.use("/admin", express.static(path.join(__dirname, "./../dist")))
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./../dist/index.html"))
+})
 
 app.listen(8080, () => {
   console.log("Server is listening on port 8080")
