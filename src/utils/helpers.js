f5830a398db98bffff8a14b8396560393e5c6599
Madhur Garg <madhur@hipbar.com>
2019-04-29 18:09:34 +0530

    query string helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,21 @@
 export function getOffsetUsingPageNo(pageNo, itemsCountPerPage = 10) {
   return itemsCountPerPage * (pageNo - 1)
 }
+
+export function getQueryParamByName(name, query = location.search.slice(1)) {
+  const queryObj = query.split("&").reduce((a, b) => {
+    if (b.split("=")[1] == 'true' || b.split("=")[1] == 'false') {
+      a[b.split("=")[0]] = JSON.parse(b.split("=")[1])
+    } else {
+      a[b.split("=")[0]] = b.split("=")[1]
+    }
+    return a
+  }, {})
+
+  return queryObj[name]
+}
+
+export function getQueryUri(queryObj) {
+  const queryUri = Object.entries(queryObj).map(obj => obj.join('=')).join('&')
+  return "?" + queryUri
+}
