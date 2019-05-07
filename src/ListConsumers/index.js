ffc90fd6a7e0fa93f69a0b79f6783c72279e2828
Madhur Garg <madhur@hipbar.com>
2019-05-07 16:15:39 +0530

    added link for gift soa

--- src/ListConsumers/index.js
+++ src/ListConsumers/index.js
@@ -1,14 +1,13 @@
 import React from "react"
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState } from "react"
 import { fetchConsumers } from "../Api"
 import Table from "Components/Table"
-import Input from "Components/Input"
+import SearchInput from "Components/SearchInput"
 import { Form } from "Components/Form"
-import Icon from "Components/Icon"
 import PageHeading from "Components/PageHeading"
 import Pagination from "react-js-pagination"
-import { getOffsetUsingPageNo, getQueryParamByName, getQueryUri } from "../utils/helpers";
-import { NavLink } from 'react-router-dom'
+import { getOffsetUsingPageNo, getQueryParamByName, getQueryUri } from "../utils/helpers"
+import { NavLink } from "react-router-dom"
 
 const tableColumns = [
   {
@@ -50,6 +49,11 @@ const tableColumns = [
     name: null,
     mapping: null,
     fn: item => <NavLink to={`/admin/consumers/notes/${item.consumer_id}`}>Notes</NavLink>
+  },
+  {
+    name: null,
+    mapping: null,
+    fn: item => item.gift_wallet_id !== 0 ? <NavLink to={`/admin/consumers/gift-soa/${item.mobile_number}`}>Gift SOA</NavLink> : ""
   }
 ]
 
@@ -70,15 +74,7 @@ export default function ListConsumers(props) {
   const [filterValue, setFilterValue] = useState(searchValue)
   const [finalFilterValue, setFinalFilterValue] = useState(searchValue)
 
-  if (filterValue.length === 0 && finalFilterValue.length) {
-    setFinalFilterValue("")
-    setActiveOffset(0)
-    setActivePage(1)
-  }
-
-  const resetFilter = () => {
-    setFinalFilterValue("")
-    setFilterValue("")
+  const reset = () => {
     props.history.push("/admin/consumers")
   }
 
@@ -107,6 +103,10 @@ export default function ListConsumers(props) {
   const fetchConsumersReq = {
     limit: limit,
     offset: activeOffset
+  }
+
+  if (filterValue.length === 0 && finalFilterValue.length) {
+    reset()
   }
 
   /** attach filter in fetchCosumerReq object if is there */
@@ -140,37 +140,31 @@ export default function ListConsumers(props) {
         <PageHeading>Consumers List ({consumersCount})</PageHeading>
         <div style={{ paddingBottom: "20px", marginLeft: "20px" }}>
           <Form onSubmit={handleFilterSubmit}>
-            <div style={{ position: "relative" }}>
-              <Input type="text" value={filterValue} onChange={(e) => { setFilterValue(e.target.value) }} placeholder="Search by email or phone.." />
-              {filterValue.length ? <Icon onClick={resetFilter} name="search--cross" /> : ""}
-            </div>
+            <SearchInput
+              reset={reset}
+              setFilterValue={setFilterValue}
+              filterValue={filterValue}
+              placeholder="Search by email or phone.."
+            />
           </Form>
         </div>
       </div>
-      {
-        isLoaded === true &&
-        <div>
-          <Table
-            data={consumers}
-            columns={tableColumns}
-          />
-          <Pagination
-            activePage={activePage}
-            itemsCountPerPage={limit}
-            totalItemsCount={consumersCount}
-            pageRangeDisplayed={5}
-            onChange={(active) => {
-              handlePageUrl(searchValue, active)
-              setActiveOffset(getOffsetUsingPageNo(active))
-              setActivePage(active)
-            }}
-          />
-        </div>
-      }
-      {
-        isLoaded === false &&
-        <h3>Loading...</h3>
-      }
+      <Table
+        data={consumers}
+        columns={tableColumns}
+        isLoaded={isLoaded}
+      />
+      <Pagination
+        activePage={activePage}
+        itemsCountPerPage={limit}
+        totalItemsCount={consumersCount}
+        pageRangeDisplayed={5}
+        onChange={(active) => {
+          handlePageUrl(searchValue, active)
+          setActiveOffset(getOffsetUsingPageNo(active))
+          setActivePage(active)
+        }}
+      />
     </div>
   )
 }
