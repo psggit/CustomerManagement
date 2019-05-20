8c9087c11950690007f36ceb01d89086995363fe
Madhur Garg <madhur@hipbar.com>
2019-05-20 17:57:53 +0530

    creating session if token is already there

--- src/App.js
+++ src/App.js
@@ -30,12 +30,18 @@ function App() {
   useEffect(() => {
     authTokenInfo()
       .then(json => {
-        if (history.location.pathname.includes("login"))
+        if (history.location.pathname.includes("login")) {
+          createSession(json)
           location.href = "/admin"
+        }
       })
       .catch(err => {
-        if (!history.location.pathname.includes("login"))
+        if (!history.location.pathname.includes("login")) {
+          err.response.json().then(json => {
+            alert(json.message)
+          })
           location.href = "/admin/login"
+        }
       })
   }, [])
 
