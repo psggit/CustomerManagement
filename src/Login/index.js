a03c7ac4ae12c85fa28f7f9fda24d59983abecfc
Madhur Garg <madhur@hipbar.com>
2019-05-20 16:59:21 +0530

    removed authinfo endpoint call

--- src/Login/index.js
+++ src/Login/index.js
@@ -23,12 +23,9 @@ export default function Login(props) {
       /** Get the token and create a session */
       authLogin(authLoginReq)
         .then(json => {
-          authTokenInfo()
-            .then(json => {
-              setLoadingState(false)
-              createSession(JSON.stringify(json))
-              props.history.push("/admin")
-            })
+          setLoadingState(false)
+          createSession(json)
+          props.history.push("/admin")
         })
         .catch(err => {
           console.log(err)
@@ -45,12 +42,12 @@ export default function Login(props) {
           <Form>
             <FormGroup>
               <label>Username</label>
-              <Input width="100%" onChange={(e) => { setUserName(e.target.value) }} />
+              <Input disabled={isLoading} width="100%" onChange={(e) => { setUserName(e.target.value) }} />
             </FormGroup>
 
             <FormGroup>
               <label>Password</label>
-              <Input width="100%" type="password" onChange={(e) => { setPassword(e.target.value) }} />
+              <Input disabled={isLoading} width="100%" type="password" onChange={(e) => { setPassword(e.target.value) }} />
             </FormGroup>
 
             <Button appearance="primary" width="100%" disabled={isLoading} onClick={handleClick}>Sign in</Button>
